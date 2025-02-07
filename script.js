f613dd8d9fa0d6de131d83fc3f87d1201c6a6168
Mike de Vries <mbv.spike@gmail.com>
2025-02-07 09:46:18 +0100

    micky single div sketch

---
+++ script.js
@@ -0,0 +1,16 @@
+// function dragEvent(event) {
+//     event.dataTransfer.setData("draggableContainer", event.target.id);
+//     event.preventDefault()
+
+// }
+
+// function drop(event){
+// event.preventDefault();
+// const data = event.dataTransfer.getData("draggableContainer");
+
+// }
+
+// function dropAllowed(event){
+// event.preventDefault();
+
+// }
